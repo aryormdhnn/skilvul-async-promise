81d88b1a531424b702b006f8bb642257737a4f70
aryormdhnn <aryoromadhon8@gmail.com>
2023-04-12 23:12:14 +0700

    update function untuk mengurutkan secara Asc dan Desc

--- modules/request/product.js
+++ modules/request/product.js
@@ -1,31 +1,50 @@
 export function getProduct() {
     const result = document.querySelector('#xhr-2-result');
-    
+    let responseJSON = []; // Menyimpan data JSON agar dapat diakses pada fungsi pengurutan
+  
     document.querySelector('#xhr-2').addEventListener('click', () => {
-        result.textContent = '';
-
-    const xhr = new XMLHttpRequest();
-
-    xhr.addEventListener('loadend', () => {
+      result.textContent = '';
+  
+      const xhr = new XMLHttpRequest();
+  
+      xhr.addEventListener('loadend', () => {
         let responseText = xhr.responseText
-        let responseJSON = JSON.parse(responseText)
+        responseJSON = JSON.parse(responseText)
         let addedHTML = ""
         for (let i=0; i<responseJSON.length; i++) {
-            addedHTML =  addedHTML + `<li>${responseJSON[i].name}</li>`
-            // console.log(responseJSON[i].name)
+          addedHTML =  addedHTML + `<li>${responseJSON[i].name}</li>`
         }
-        console.log(addedHTML)
         result.innerHTML = `<ul>${addedHTML}</ul>`
-
-        // result.textContent = `${result.textContent}Finished with status: ${xhr.responseText}`;
+      });
+  
+      xhr.open('GET', 'https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+      xhr.send();
+      result.textContent = `${result.textContent}Started XHR request\n`;
     });
-
-    xhr.open('GET', 'https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
-    xhr.send();
-    result.textContent = `${result.textContent}Started XHR request\n`;});
-
+  
+    // Menambahkan button untuk mengurutkan secara ascending (A-Z)
+    document.querySelector('#sort-asc').addEventListener('click', () => {
+      responseJSON.sort((a, b) => a.name.localeCompare(b.name))
+      let addedHTML = ""
+      for (let i=0; i<responseJSON.length; i++) {
+        addedHTML =  addedHTML + `<li>${responseJSON[i].name}</li>`
+      }
+      result.innerHTML = `<ul>${addedHTML}</ul>`
+    });
+  
+    // Menambahkan button untuk mengurutkan secara descending (Z-A)
+    document.querySelector('#sort-desc').addEventListener('click', () => {
+      responseJSON.sort((a, b) => b.name.localeCompare(a.name))
+      let addedHTML = ""
+      for (let i=0; i<responseJSON.length; i++) {
+        addedHTML =  addedHTML + `<li>${responseJSON[i].name}</li>`
+      }
+      result.innerHTML = `<ul>${addedHTML}</ul>`
+    });
+  
     document.querySelector('#reload-2').addEventListener('click', () => {
-        result.textContent = '';
-        document.location.reload();
+      result.textContent = '';
+      document.location.reload();
     });
-}
+  }
+  
